67870810949e71d29cab615a8c3f2be898fe5d97
Shajahan Mohamed Sadam <91425409+Sadam42@users.noreply.github.com>
2022-01-15 00:58:09 +0100

    w

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ var doge = document.getElementById("dogecoin");
 var setting = {
     "async": true,
     "scrossDomain":true,
-    "url":"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=USD",
+    "url":"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
     "method": "GET",
     "headers": {}
 
